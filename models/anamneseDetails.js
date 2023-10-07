dad77b0c7557d4ce0a6016f2f9da6318c0dbb62f
Natália Resina <resina.natalia@gmail.com>
2023-10-07 18:47:07 -0300

    model for anamnese details

--- models/anamneseDetails.js
+++ models/anamneseDetails.js
@@ -1,11 +1,11 @@
 const db = require('../util/database');
 
 module.exports = class AnamneseDetails {
-    constructor(dentista){        
-        this.dentista = dentista;       
+    constructor(id){        
+        this.id = id;       
     }
 
-    static findAnamneseDetails(dentista){
-        return db.execute('SELECT A.id, A.dentista, A.paciente, A.data, AN.anamnese, AN.pergunta, AN.resposta, P.descricao AS pergunta_descricao, R.descricao AS pergunta_resposta FROM anm_pergunta AS P INNER JOIN anm_anamnese_pergunta_resposta AS AN ON P.id = AN.pergunta INNER JOIN anm_resposta AS R ON R.id = AN.resposta INNER JOIN anm_anamnese AS A ON A.id = AN.anamnese WHERE dentista = ?', [dentista]);
+    static findAnamneseDetails(id){
+        return db.execute('SELECT A.id, A.dentista, A.paciente, A.data, AN.anamnese, AN.pergunta, AN.resposta, P.descricao AS pergunta_descricao, R.descricao AS pergunta_resposta FROM anm_pergunta AS P INNER JOIN anm_anamnese_pergunta_resposta AS AN ON P.id = AN.pergunta INNER JOIN anm_resposta AS R ON R.id = AN.resposta INNER JOIN anm_anamnese AS A ON A.id = AN.anamnese WHERE anamnese = ?', [id]);
     }
 };
