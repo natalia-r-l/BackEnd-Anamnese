bb465192470ac61ef068f638d876e68dc09471ae
Natália Resina <resina.natalia@gmail.com>
2023-10-07 20:13:15 -0300

    password validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,12 +7,19 @@ const Login = require('../models/login');
 exports.login = async(req,res,next) => {
 
     const dentista = req.body.dentista;    
+    const password = req.body.password;
 
     try{        
-        const anamnese = await Login.loginDentista(dentista);
+        const anamnese = await Login.loginDentista(dentista, password);
 
         if(anamnese[0].length < 1) {
             const error = new Error('Dentista não encontrado!');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        if(dentista !== password) {
+            const error = new Error('Credenciais inválidas!');
             error.statusCode = 401;
             throw error;
         }
@@ -21,7 +28,7 @@ exports.login = async(req,res,next) => {
 
         const token = jwt.sign(
             {
-              dentista: storedAnamnese.dentista,
+              dentista: storedAnamnese.dentista,              
               anamneseId: storedAnamnese.id,
             },
             'secretfortoken',
