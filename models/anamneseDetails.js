24d5424bfedd7059fa2bb04962ca04c10a054b2b
Natália Resina <resina.natalia@gmail.com>
2023-10-09 12:47:40 -0300

    refactoring

--- models/anamneseDetails.js
+++ models/anamneseDetails.js
@@ -6,6 +6,6 @@ module.exports = class AnamneseDetails {
     }
 
     static findAnamneseDetails(id){
-        return db.execute('SELECT A.id, A.dentista, A.paciente, A.data, AN.anamnese, AN.pergunta, AN.resposta, P.descricao AS pergunta_descricao, R.descricao AS pergunta_resposta FROM anm_pergunta AS P INNER JOIN anm_anamnese_pergunta_resposta AS AN ON P.id = AN.pergunta INNER JOIN anm_resposta AS R ON R.id = AN.resposta INNER JOIN anm_anamnese AS A ON A.id = AN.anamnese WHERE anamnese = ?', [id]);
+        return db.execute('SELECT A.id, A.paciente, A.data, AN.pergunta, P.descricao AS pergunta_descricao, R.descricao AS pergunta_resposta FROM anm_pergunta AS P INNER JOIN anm_anamnese_pergunta_resposta AS AN ON P.id = AN.pergunta INNER JOIN anm_resposta AS R ON R.id = AN.resposta INNER JOIN anm_anamnese AS A ON A.id = AN.anamnese WHERE anamnese = ?', [id]);
     }
 };
